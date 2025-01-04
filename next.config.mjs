9634e3c228cace326eb4dc6dbdc07c96ade269b7
MSulemanBhanbhro <ms5901562@gmail.com>
2025-01-04 16:53:15 +0500

    change domain name for images

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,10 @@ const nextConfig = {
                 hostname:"fakestoreapi.com",
             }
         ]
-    }
+    },
+    images: {
+        domains: ['fakestoreapi.com'], // Add the external image domain here
+      },
 };
 
 export default nextConfig;
